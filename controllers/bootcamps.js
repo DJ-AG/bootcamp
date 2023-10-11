88f6931e052e19bdabb7e3170efee94e2dd62fa2
Alexei Golovanov <alekseigolovanov89@gmail.com>
2023-10-11 08:33:22 +0300

    added advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,7 +8,36 @@ const ErrorResponse = require('../utils/errorResponse');
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async(req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+    
+  let query;
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if(req.query.select){
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Executing query
+  const bootcamps = await query;
 
   res.status(200).json({ success: true, data: bootcamps });
 })
@@ -17,6 +46,7 @@ exports.getBootcamps = asyncHandler(async(req, res, next) => {
 // @route GET /api/v1/bootcamps/:id
 // @access Public
 exports.getBootcampById = asyncHandler(async(req, res, next) => {
+
   const bootcamp = await Bootcamp.findById(req.params.id);
 
   if(!bootcamp) return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
