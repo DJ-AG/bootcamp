479c67bbb76a66b2fa3cd4bd89dbf2e61938bfbb
Alexei Golovanov <alekseigolovanov89@gmail.com>
2023-10-12 15:42:19 +0300

    added user model

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,27 +9,44 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+// Import the Bootcamp model for query middleware
 const Bootcamp = require('../models/Bootcamp');
+
+// Import the middleware for advanced query results
 const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
 
+// Initialize the express router
 const router = express.Router();
 
 // Re-route into other resource routers
+// Any requests to '/:bootcampId/courses' will be forwarded to the courseRouter
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/radius/:zipcode/:distance').get(getBootcampsRadius)
+// Route for getting bootcamps in a specific radius based on a zipcode and distance.
+// The route parameters ":zipcode" and ":distance" are dynamic and user-defined in the URL.
+router.route('/radius/:zipcode/:distance').get(getBootcampsRadius);
 
-router.route('/').get(advancedResults(Bootcamp,'courses'),getBootcamps).post(createBootcamp);
+// Define the routes for general bootcamp data retrieval and creation.
+// Apply the advancedResults middleware for GET requests, enhancing query capabilities.
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)  // "courses" is populated in query
+  .post(createBootcamp);  // Route to create a new bootcamp
 
+// Define route to handle bootcamp photo uploads.
+// ":id/photo" is a dynamic route that expects an ID parameter and handles photo upload via PUT request.
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
+// Define the routes to get, update, and delete a specific bootcamp by its ID.
+// ":id" is a dynamic parameter, allowing for various IDs to be input and passed to controller functions.
 router
   .route('/:id')
-  .get(getBootcampById)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .get(getBootcampById)  // Route to get a bootcamp by ID
+  .put(updateBootcamp)   // Route to update a bootcamp by ID
+  .delete(deleteBootcamp);  // Route to delete a bootcamp by ID
 
+// Export the router
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -1,25 +1,31 @@
 const express = require('express');
-
 const {
     getCourses,
     getCourseById,
     addCourse,
     updateCourse,
     deleteCourse
-    } = require('../controllers/courses');
+} = require('../controllers/courses');
 
-// Include other resource routers
-
+// Initializing the express router.
+// The {mergeParams: true} option allows the router to access parameters from parent routers.
 const router = express.Router({mergeParams: true});
 
-router.route('/').get(getCourses).post(addCourse)
+// Define the base routes for retrieving all courses and adding a new course.
+// .get(getCourses) - Handles GET requests to retrieve all courses.
+// .post(addCourse) - Handles POST requests to create a new course.
+router.route('/').get(getCourses).post(addCourse);
 
+// Define routes to manage specific courses by ID.
+// ":id" is a dynamic parameter, representing the ID of a specific course.
+// .get(getCourseById) - Handles GET requests to retrieve a specific course by ID.
+// .delete(deleteCourse) - Handles DELETE requests to remove a specific course by ID.
+// .put(updateCourse) - Handles PUT requests to update details of a specific course by ID.
 router
     .route('/:id')
     .get(getCourseById)
     .delete(deleteCourse)
-    .put(updateCourse)
+    .put(updateCourse);
 
-
-
+// Export the router to be used in other parts of the application.
 module.exports = router;
